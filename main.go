0874b21551829c227c4566ecc9e7081d6c048efd
zjy-1111 <1063145752@qq.com>
2019-07-10 11:42:28 +0800

    平滑关闭服务

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"rehabilitation_prescription/pkg/setting"
 	"rehabilitation_prescription/routers"
+	"time"
 )
 
 func main() {
@@ -18,5 +23,25 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	// 收到中断信号不退出
+	quit := make(chan os.Signal)      // 信号量通道
+	signal.Notify(quit, os.Interrupt) // 捕获信号
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
